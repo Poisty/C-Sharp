c15ea1ce50a257c1532bd62b193705340350ee43
Fredrik Augustinus Villo <fredrik.villo@gmail.com>
2025-03-19 10:06:16 +0100

    ryddet i repo

--- AnimalMatchingGame/AnimalMatchingGame/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace AnimalMatchingGame;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- PickRandomCardsMAUI/PickRandomCardsMAUI/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PickRandomCardsMAUI;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
