c883da1f671ca85590fa50741f84d204f681e2b9
Fredrik Augustinus Villo <fredrik.villo@gmail.com>
2025-03-19 10:01:48 +0100

    fjerner ubrukte filer

--- endaentest/testigjen/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace testigjen;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
