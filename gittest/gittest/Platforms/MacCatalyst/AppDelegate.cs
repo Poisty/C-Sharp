0ebf3424c45734499da41e8635331fffb2dec53a
Fredrik Augustinus Villo <fredrik.villo@gmail.com>
2025-03-19 10:04:03 +0100

    remove gittest

--- gittest/gittest/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace gittest;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
